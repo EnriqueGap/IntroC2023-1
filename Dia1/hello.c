f2731f3bd487e5657136ce2abf4e309cf0cb276f
Enrique Galicia <enrique.g@ciencias.unam.mx>
2022-09-17 20:20:10 -0500

    Mi primer programa en C

---
+++ Dia1/hello.c
@@ -0,0 +1,14 @@
+/*
+* hello programa para imprimir por pantalla "Hola Mundo!" *
+* Autor: Enrique Galicia 				  *
+* Fecha: 						  *
+* Uso: Ejecuta el programa y el mensaje aparecerá 	  *
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	//Imprimimos por pantalla nuestro mensaje
+	printf("Hola Mundo!");
+}
