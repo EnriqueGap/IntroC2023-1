0473efa99a4c8545aa94300efeca2df08c8fc7b2
Enrique Galicia <enrique.g@ciencias.unam.mx>
2022-09-22 03:30:17 -0500

    +

--- Dia2/Funciones/rk.py
+++ Dia2/Funciones/rk.py
@@ -5,6 +5,4 @@ data1=pd.read_csv('rk_yp.dat',sep="\t",header=None)
 #data2=pd.read_csv('rk_ypp.dat',sep="\t",header=None)
 
 plt.plot(data1.iloc[:,0].values,data1.iloc[:,1].values)
-plt.plot(data1.iloc[:,0].values,data1.iloc[:,2].values)
-plt.plot(data1.iloc[:,0].values,data1.iloc[:,3].values)
 plt.savefig('rk_methods.png')
